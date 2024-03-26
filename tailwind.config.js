706cbd270d479ebf6b6451df61fbeea9fbc75189
Carlos <rocarlos1205@gmail.com>
2024-03-25 20:03:24 -0500

    Configuración de Tailwind y .gitignore actualizadas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './**/templates/*.html'
+    './**/templates/*.html',
+    './**/templates/partials/*.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '320x566': {'raw': '(width: 320px) and (height: 566px)'}
+      }
+    },
   },
   plugins: [],
 }
